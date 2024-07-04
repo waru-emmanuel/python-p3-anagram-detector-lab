fc08f0af3aa4bebafa09a246b8071ed3e4e98c99
EMMANUEL <waruterewachira7@gmail.com>
2024-07-04 23:32:35 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+#my anagram detector
+#You'll need to iterate over the list of words that the match() method takes as an argument. You will compare each word of that list to the word that the Anagram class is initialized with.
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = []
+        for word in words:
+            if sorted(self.word) == sorted(word):
+                matches.append(word)
+        return matches
+
